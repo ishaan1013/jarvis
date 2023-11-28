af6e5424ba834772026baf891742b5758d3eacfd
Ishaan Dey <ishaan1013@gmail.com>
2023-11-28 09:15:23 -0500

    state actions

--- frontend/lib/state.ts
+++ frontend/lib/state.ts
@@ -1,9 +1,72 @@
 import { create } from "zustand";
 
-type Visible = {
-  porsche: boolean;
-  ironman: boolean;
-  goose: boolean;
+// type Visible = {
+//   porsche: boolean;
+//   ironman: boolean;
+//   goose: boolean;
+// };
+
+type ObjectData = {
+  position: {
+    x: number;
+    y: number;
+  };
+  // need to remove one
+  rotation: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  scale: number;
+  visible: boolean;
+};
+
+type ModelName = "porsche" | "ironman" | "goose";
+
+type Objects = {
+  [key in ModelName]: ObjectData;
+};
+
+const initialObjects = {
+  porsche: {
+    position: {
+      x: 0,
+      y: 0,
+    },
+    rotation: {
+      x: 0.2,
+      y: 4.5,
+      z: 0,
+    },
+    scale: 0.3,
+    visible: true,
+  },
+  ironman: {
+    position: {
+      x: 0,
+      y: -0.5,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    scale: 0.6,
+    visible: false,
+  },
+  goose: {
+    position: {
+      x: 0,
+      y: -0.1,
+    },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    scale: 2,
+    visible: false,
+  },
 };
 
 type State = {
@@ -11,8 +74,17 @@ type State = {
   setTarget: (target: THREE.Object3D | null) => void;
   menuOpen: boolean;
   setMenuOpen: (menuOpen: boolean) => void;
-  visible: Visible;
-  setVisible: (visible: Visible) => void;
+  objects: Objects;
+  setVisible: (modelName: ModelName, visible: boolean) => void;
+  setPosition: (
+    modelName: ModelName,
+    position: { x: number; y: number },
+  ) => void;
+  setRotation: (
+    modelName: ModelName,
+    rotation: { x: number; y: number; z: number },
+  ) => void;
+  setScale: (modelName: ModelName, scale: number) => void;
 };
 
 export const useStore = create<State>((set) => ({
@@ -20,10 +92,45 @@ export const useStore = create<State>((set) => ({
   setTarget: (target) => set({ target }),
   menuOpen: true,
   setMenuOpen: (menuOpen) => set({ menuOpen }),
-  visible: {
-    porsche: true,
-    ironman: false,
-    goose: false,
-  },
-  setVisible: (visible) => set({ visible }),
+  objects: initialObjects,
+  setVisible: (modelName, visible) =>
+    set((state) => ({
+      objects: {
+        ...state.objects,
+        [modelName]: {
+          ...state.objects[modelName],
+          visible,
+        },
+      },
+    })),
+  setPosition: (modelName, position) =>
+    set((state) => ({
+      objects: {
+        ...state.objects,
+        [modelName]: {
+          ...state.objects[modelName],
+          position,
+        },
+      },
+    })),
+  setRotation: (modelName, rotation) =>
+    set((state) => ({
+      objects: {
+        ...state.objects,
+        [modelName]: {
+          ...state.objects[modelName],
+          rotation,
+        },
+      },
+    })),
+  setScale: (modelName, scale) =>
+    set((state) => ({
+      objects: {
+        ...state.objects,
+        [modelName]: {
+          ...state.objects[modelName],
+          scale,
+        },
+      },
+    })),
 }));
