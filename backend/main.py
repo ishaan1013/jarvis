717e77441e457b24f65d522658c7ef72e3580988
Ishaan Dey <ishaan1013@gmail.com>
2023-12-02 17:36:06 -0500

    working gestures

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,9 @@ import tensorflow as tf
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+import time
+import math
+
 BaseOptions = mp.tasks.BaseOptions
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
@@ -13,57 +16,70 @@ GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-# def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-#     print('gesture recognition result: {}'.format(result))
+def calculate_distance(x1, y1, x2, y2):
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
 
 
-# options = GestureRecognizerOptions(
-#     base_options=BaseOptions(model_asset_path='gesture_recognizer.task'),
-#     running_mode=VisionRunningMode.LIVE_STREAM,
-#     result_callback=print_result)
-# with GestureRecognizer.create_from_options(options) as recognizer:
+# Data points for a polynomial regression
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+coff = np.polyfit(x, y, 2)
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
 
-hands = mp_hands.Hands(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.7,
-    max_num_hands=2,
-)
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    print('gesture recognition result: {} at {}'.format(
+        result.gestures[0] if len(result.gestures) > 0 else [], timestamp_ms))
 
-webcam = cv2.VideoCapture(0)
 
-while webcam.isOpened():
-    ret, frame = webcam.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path='gesture_recognizer.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+with GestureRecognizer.create_from_options(options) as recognizer:
 
-    frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
-    results = hands.process(frame)
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    frame_height, frame_width, _ = frame.shape
+    hands = mp_hands.Hands(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.7,
+        max_num_hands=2,
+    )
 
-    # mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-    # recognizer.recognize_async(mp_image, webcam.get(cv2.CAP_PROP_POS_MSEC))
+    webcam = cv2.VideoCapture(0)
 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
+    while webcam.isOpened():
+        ret, frame = webcam.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
 
-            for ids, landmrk in enumerate(hand_landmarks.landmark):
-                cx, cy = landmrk.x * frame_width, landmrk.y*frame_height
+        frame = cv2.cvtColor(cv2.flip(frame, 0), cv2.COLOR_BGR2RGB)
+        results = hands.process(frame)
 
-            # print(hand_landmarks.landmark[5].x * 100,
-            #       hand_landmarks.landmark[5].y * 100)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        frame_height, frame_width, _ = frame.shape
 
-            mp_drawing.draw_landmarks(
-                frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        recognizer.recognize_async(
+            mp_image, mp.Timestamp.from_seconds(time.time()).value)
 
-    cv2.imshow('MediaPipe Hands', frame)
-    if cv2.waitKey(5) & 0xFF == ord("q"):
-        break
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
 
-webcam.release()
-cv2.destroyAllWindows()
+                for ids, landmrk in enumerate(hand_landmarks.landmark):
+                    cx, cy = landmrk.x * frame_width, landmrk.y*frame_height
+
+                # print(hand_landmarks.landmark[5].x * 100,
+                #       hand_landmarks.landmark[5].y * 100)
+
+                mp_drawing.draw_landmarks(
+                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow('MediaPipe Hands', frame)
+        if cv2.waitKey(5) & 0xFF == ord("q"):
+            break
+
+    webcam.release()
+    cv2.destroyAllWindows()
